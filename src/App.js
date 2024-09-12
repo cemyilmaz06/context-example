db9318f44ba76d35178156765413aaec4a279003
cem <cemyilmaztech@gmail.com>
2024-09-12 22:56:44 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ export const StudentContext=createContext()
 
 function App() {
 const [student , setStudent  ] = useState(data)
+const changeColor=(id,newRenk)=>(
+  setStudent(student.map((a)=>a.id===id ? {...a, color:newRenk} : a))
+)
 
   return (
     <div>
-     <StudentContext.Provider value={{student}}>
+     <StudentContext.Provider value={{student,changeColor}}>
       <Home/>
      </StudentContext.Provider>
     </div>
--- src/components/OgrenciItem.jsx
+++ src/components/OgrenciItem.jsx
@@ -3,15 +3,15 @@ import { StudentContext } from '../App'
 
 const OgrenciItem = () => {
 
-    const {student}=useContext(StudentContext)
+    const {student,changeColor}=useContext(StudentContext)
   return (
     <div>
         {student.map((a)=>(
-            <div key={a.id}>
+            <div key={a.id} style={{background:a.color}}>
                  <h3>NAME: {a.name}</h3>
                  <h4>EMAIL: {a.email} </h4>
                  <h4>AGE: {a.age} </h4>
-                 COLOR: <input type="text" />
+                 COLOR: <input type="text" value={a.color} onChange={(e)=>changeColor(a.id, e.target.value)}/>
             </div>
            
         ))}
